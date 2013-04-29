5f8cb85712c2bf89615d017aeea023d7301d2e14
Igor Melao <igormelao@gmail.com>
2013-04-29 18:25:03 -0300

    Criação do Projeto

---
+++ MeuPrimeiroApp/MeuPrimeiroAppAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MeuPrimeiroAppAppDelegate.h
+//  MeuPrimeiroApp
+//
+//  Created by Igor Gonçalves Caballero Melao on 29/04/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MeuPrimeiroAppAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MeuPrimeiroApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MeuPrimeiroApp
+//
+//  Created by Igor Gonçalves Caballero Melao on 29/04/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MeuPrimeiroAppAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MeuPrimeiroAppAppDelegate class]));
+    }
+}
